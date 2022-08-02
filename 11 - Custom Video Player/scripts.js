a7cfab6e0bc7ed694006012724810b6746fab201
Mamadou Sylla <m27sylla@gmail.com>
2022-08-02 14:48:00 -0400

    progressbar moving with video duration done, seeking with mouse to do

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,7 +2,7 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector(".progress__filled");
+const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.getElementsByClassName('skip');
 const ranges = player.querySelectorAll('.player__slider');
@@ -31,10 +31,24 @@ function handleRangeUpdate(){
 	console.log(this.name);
 	video[this.name] = this.value;
 }
+
+function handleProgress(){
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e){
+const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+video.currentTime = scrubTime;
+console.log(e);
+}
+
+
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play',updateButton);
 video.addEventListener('pause',updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 
@@ -44,3 +58,5 @@ Array.from(skipButtons).forEach(button =>
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+progress.addEventListener('click', scrub);
